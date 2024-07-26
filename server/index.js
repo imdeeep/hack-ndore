9e627f853ce6bd6000c8c0d50305f3643399c378
imdeeep <mandeep7yadav@gmail.com>
2024-07-26 23:01:11 +0530

    serverAdd

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log("working...")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors"); 
+const connectDB = require("./config/db");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
